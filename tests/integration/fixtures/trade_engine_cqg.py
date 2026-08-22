5cfbc4b5928f17a336160522dd7ce3085a85c4ed
Dexter <dex-hon-sci@outlook.com>
2026-08-23 01:19:39 +0800

    Controller class added, finishing all prechecks

--- tests/integration/fixtures/trade_engine_cqg.py
+++ tests/integration/fixtures/trade_engine_cqg.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+
 from EC_API.channel.base import Channel
 from EC_API.channel.redis import RedisChannel
 from EC_API.connect.base import Connect
@@ -12,13 +13,85 @@ from EC_API.payload.base import Payload, ExecutePayload
 from EC_API.payload.safety import PreTradeRiskCheck
 from EC_API.recorders.base import SQLSchemaTable, Recorder
 from EC_API.recorders.sqlite_recorder import SQLiteRecorder
-from EC_API.exceptions import ChannelMissingSettingError
+from EC_API.exceptions import (
+    ChannelMissingSettingError, 
+    TradeSessionRequestError,
+    TradeSessionTimeOutError
+    )
 
 HOST_NAME, USR_NAME, PASSWORD, ACCOUNT_ID = 0, 0, 0, 0
 
 logger = logging.getLogger(__name__)
 
 # Controller class??
+from typing import Prototype, Optional, Callable, Any
+
+ALLOWED_SUB_SCOPE = ["order_info"]
+
+class Controller(Prototype):...
+
+class TradeEngineController(Controller):
+    def __init__(
+            self, 
+            trade_session: TradeSession, 
+            channel: Channel, 
+            pretrade_risk_check: PreTradeRiskCheck
+        ):
+        self._trade_session = trade_session
+        self._channel = channel
+        self._ptrc = pretrade_risk_check
+        self.SCOPE_MAP = {"order_info": SubScope.ORDERS}
+        
+    def add_in_stream(
+            self, in_stream_name: str,
+            task, container,
+            callback: Optional[Callable[Any, None]] = None
+        ) -> None:
+
+        if in_stream_name in self._channel.in_streams:
+            return
+        
+        # Format Check
+        sub_scope = in_stream_name.split(":")[0] 
+        symbol_name = in_stream_name.split(":")[1]
+
+        # See if pre-trade risk para is present
+        self._ptrc.
+        # Before adding, either ask for pre-trade risk parameters or load a preset
+        try:
+            # do trade subscription if scope is not present
+            if self.SCOPE_MAP.get(sub_scope) not in self._trade_session._active_trade_subs.values():
+                next_sub_id = list(self._trade_session._active_trade_subs.keys())[-1] + 1
+                self.trade_session.trade_subscription_request(next_sub_id, sub_scope)
+            
+            # Symbol Resolution
+            if not self._trade_session.has_symbol(symbol_name):
+                self._trade_session.resolve_symbol(symbol_name)
+                
+            # Add in_stream, add symbols, 
+            self._channel.in_streams.add(in_stream_name)
+                    
+            if callback:
+                # For each new in_stream added, make a new async task send_loop
+                callback(in_stream_name)
+                
+        except (TradeSessionRequestError, TradeSessionTimeOutError) as e:
+            logger.error(str(e))
+
+    def remove_in_stream(self, in_stream_name: str) -> None:
+        # unsub trade
+        
+        # remove out_stream
+        task = self._tasks.pop(in_stream_name, None)
+        if task is not None:
+            task.cancel()
+            try:
+                await task          # await ONLY here — to let CancelledError settle
+            except asyncio.CancelledError:
+                pass
+        self.channel.in_streams.discard(in_stream_name)
+        self.channel.last_ids.pop(in_stream_name, None)   
+        
 
 class TradeEngineCQG:
     def __init__(
@@ -54,63 +127,10 @@ class TradeEngineCQG:
         # ---- Channel ----
         self.CTRL_STREAM: str = "CMD:Trade_Engine"
         
-    # ------- Manual control methods 
-    def add_in_stream(self, in_stream_name: str) -> None:
-        if in_stream_name in self.channel.in_streams:
-            return
-        
-        # See if pre-trade risk para is present
-        
-        # Before adding, either ask for pre-trade risk parameters or load a preset
-        
-        try:
-            # do trade subscription
-            self.trade_session.trade_subscription_request(sub_id, sub_scope)
-            # Add in_stream, add symbols, 
-            self.channel.in_streams.add(in_stream_name)
+        self.controller: Controller = TradeEngineController(
+            self.trade_session, self.channel
+            )
                     
-            # For each new in_stream added, make a new async task send_loop
-            self._send_order_tasks[in_stream_name] = asyncio.create_task(
-                self._send_order_loop(in_stream_name)
-                )
-        except:
-            ...
-
-    def remove_in_stream(self, in_stream_name: str) -> None:
-        # unsub trade
-        
-        # remove out_stream
-        task = self._tasks.pop(in_stream_name, None)
-        if task is not None:
-            task.cancel()
-            try:
-                await task          # await ONLY here — to let CancelledError settle
-            except asyncio.CancelledError:
-                pass
-        self.channel.in_streams.discard(in_stream_name)
-        self.channel.last_ids.pop(in_stream_name, None)   
-        
-
-    # ------- Controls
-    async def _control_loop(self):
-        # cmd: add_stream, remove_stream, start_engine, stop_engine
-        while not self._stop_evt.is_set():
-            try:
-                # Listen to control command and add/remove in_stream
-                cmd = await self.channel.listen(stream_name=self.CTRL_STREAM, data_name="")
-                
-                match cmd[0]: # ("ADD", "order_info:WTI")
-                    case "ADD":
-                        self.add_in_stream(cmd[1])
-                    case "REMOVE":
-                        self.remove_in_stream(cmd[1])
-                    case _:
-                        logger.warning("[Trade Engine] Unknown Command: %s", cmd[0])
-            except asyncio.CancelledError:
-                raise
-            except Exception as e:
-                logger.error("control_loop error: %s", e, exc_info=True)
-            
     # ------- Engine functions (Trade)
     async def _package_and_send(
         self, 
@@ -122,7 +142,14 @@ class TradeEngineCQG:
           check_method = self.PREC # Static risk check done upon creation
           )
         await ExecutePayload(live_order=LiveOrderCQG(trade_session)).unload(PL)
-
+        
+        
+    def _add_send_task_to_map(self, in_stream_name: str):
+        # For each new in_stream added, make a new async task send_loop
+        self._send_order_tasks[in_stream_name] = asyncio.create_task(
+            self._send_order_loop(in_stream_name)
+            )
+        
     async def _send_order_loop(self, in_stream_name: str) -> None:
         while not self._stop_evt.is_set():
             # Continuous listening to the latest order instruction from redis stream
@@ -135,7 +162,28 @@ class TradeEngineCQG:
             order_type, order_info = msg
             await self._package_and_send(order_type, order_info)
             
-        
+    # ------- Controls
+    async def _control_loop(self):
+        # cmd: add_stream, remove_stream, start_engine, stop_engine
+        while not self._stop_evt.is_set():
+            try:
+                # Listen to control command and add/remove in_stream
+                cmd = await self.channel.listen(stream_name=self.CTRL_STREAM, data_name="")
+                
+                match cmd[0]: # ("ADD", "order_info:WTI")
+                    case "ADD":
+                        self.controller.add_in_stream(
+                            cmd[1], callback = self._add_send_task_to_map
+                            )
+                    case "REMOVE":
+                        self.controller.remove_in_stream(cmd[1])
+                    case _:
+                        logger.warning("[Trade Engine] Unknown Command: %s", cmd[0])
+            except asyncio.CancelledError:
+                raise
+            except Exception as e:
+                logger.error("control_loop error: %s", e, exc_info=True)
+
     # -------- Engine LifeCycle
     async def _setup(self):
         # Connect to channel
@@ -146,6 +194,8 @@ class TradeEngineCQG:
             
         # start trade session
         self.trade_session.start()
+        
+        # start control loop
         
         # subscribe all the trade subscriptions and pre-resolve symbols
         for stream_name in self.channel.in_streams:
