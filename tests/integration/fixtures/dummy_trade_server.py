238d37e889dc4a65f27fdc14ab62f634faff69f9
Dexter <dex-hon-sci@outlook.com>
2026-09-04 17:32:05 +0800

    session logic WIP

--- tests/integration/fixtures/dummy_trade_server.py
+++ tests/integration/fixtures/dummy_trade_server.py
@@ -6,3 +6,10 @@ Created on Fri Jul  3 21:08:40 2026
 @author: dexter
 """
 
+class DummyDataServer:
+    def __init__(self):
+        self.response_logic = None
+        self.ws_server = None
+    # tape import task
+    # streaming ticks base on tape
+    # transfer those thorugh the websocket
